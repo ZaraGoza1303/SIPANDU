8d519fb2c89f0af51a403e40bc13c2e1558a2265
karlinna <karlinayuw@gmail.com>
2026-06-24 22:58:36 +0700

    wip: main content kanan belum diisi

---
+++ frontend/app/dashboard/page.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+export default function DashboardPage() {
+    return (
+        <div className="flex min-h-screen bg-white-100 text-gray-800">
+            <h1 className="font-bold">nnti yh</h1>
+            <br />
+            <p>ini harus di isi main content kita yg kayak data statistik posyandu</p>
+        </div>
+    )
+}

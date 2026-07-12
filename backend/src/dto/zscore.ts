870c29761f0c170f80df3b63e3f6a4671952b579
ZaraGoza1303 <mantapucoco@gmail.com>
2026-07-12 17:34:02 +0700

    feat: add docker

--- backend/src/dto/zscore.ts
+++ backend/src/dto/zscore.ts
@@ -29,44 +29,44 @@ function zScoreLMS(X: number, L: number, M: number, S: number): number {
   return (Math.pow(X / M, L) - 1) / (L * S);
 }
 
-export function calculateZScoreWHO(weight: number, height: number, age_months: number, gender: string): ZScoreOutput{
-    const genderKey = gender === 'L' ? 'laki_laki' : 'perempuan';
-    const dataGender = whoData[genderKey as keyof typeof whoData];
+export function calculateZScoreWHO(weight: number, height: number, age_months: number, gender: string): ZScoreOutput {
+  const genderKey = (gender === 'L' || gender === 'Laki-Laki') ? 'laki_laki' : 'perempuan';
+  const dataGender = whoData[genderKey as keyof typeof whoData];
 
-    const umurKey = age_months.toString();
-    const acuan = dataGender[umurKey as keyof typeof dataGender];
+  const umurKey = age_months.toString();
+  const acuan = dataGender[umurKey as keyof typeof dataGender];
 
-    if (!acuan) {
-        throw new Error(`Data umur ${age_months} bulan tidak ditemukan.`);
-    }
+  if (!acuan) {
+    throw new Error(`Data umur ${age_months} bulan tidak ditemukan.`);
+  }
 
-    const wfa = zScoreLMS(weight, acuan.wfa.L, acuan.wfa.M, acuan.wfa.S);
-    const hfa = zScoreLMS(height, acuan.hfa.L, acuan.hfa.M, acuan.hfa.S);
-    const wfh = zScoreLMS(weight, acuan.wfh.L, acuan.wfh.M, acuan.wfh.S);
+  const wfa = zScoreLMS(weight, acuan.wfa.L, acuan.wfa.M, acuan.wfa.S);
+  const hfa = zScoreLMS(height, acuan.hfa.L, acuan.hfa.M, acuan.hfa.S);
+  const wfh = zScoreLMS(weight, acuan.wfh.L, acuan.wfh.M, acuan.wfh.S);
 
-    let stuntingStatus: StuntingStatus = StuntingStatus.Normal;
-    let wastingStatus: WastingStatus = WastingStatus.GiziBaikNormal;
-    let underweightStatus: UnderweightStatus = UnderweightStatus.BeratBadanNormal;
+  let stuntingStatus: StuntingStatus = StuntingStatus.Normal;
+  let wastingStatus: WastingStatus = WastingStatus.GiziBaikNormal;
+  let underweightStatus: UnderweightStatus = UnderweightStatus.BeratBadanNormal;
 
-    if (hfa < -3) stuntingStatus = StuntingStatus.SeverelyStunted;
-    else if (hfa < -2) stuntingStatus = StuntingStatus.Stunted;
-    else if (hfa > 3) stuntingStatus = StuntingStatus.High;
+  if (hfa < -3) stuntingStatus = StuntingStatus.SeverelyStunted;
+  else if (hfa < -2) stuntingStatus = StuntingStatus.Stunted;
+  else if (hfa > 3) stuntingStatus = StuntingStatus.High;
 
-    if (wfh < -3) wastingStatus = WastingStatus.GiziBurukSeverelyWasted;
-    else if (wfh < -2) wastingStatus = WastingStatus.GiziKurangWasted;
-    else if (wfh > 1) wastingStatus = WastingStatus.BerisikoGiziLebih;
+  if (wfh < -3) wastingStatus = WastingStatus.GiziBurukSeverelyWasted;
+  else if (wfh < -2) wastingStatus = WastingStatus.GiziKurangWasted;
+  else if (wfh > 1) wastingStatus = WastingStatus.BerisikoGiziLebih;
 
-    if (wfa < -3) underweightStatus = UnderweightStatus.BeratBadanSangatKurang;
-    else if (wfa < -2) underweightStatus = UnderweightStatus.BeratBadanKurang;
+  if (wfa < -3) underweightStatus = UnderweightStatus.BeratBadanSangatKurang;
+  else if (wfa < -2) underweightStatus = UnderweightStatus.BeratBadanKurang;
 
-    const res: ZScoreOutput = {
-        wfa: Math.round(wfa * 100) / 100,
-        hfa: Math.round(hfa * 100) / 100,
-        wfh: Math.round(wfh * 100) / 100,
-        stuntingStatus,
-        wastingStatus,
-        underweightStatus,
-    }
+  const res: ZScoreOutput = {
+    wfa: Math.round(wfa * 100) / 100,
+    hfa: Math.round(hfa * 100) / 100,
+    wfh: Math.round(wfh * 100) / 100,
+    stuntingStatus,
+    wastingStatus,
+    underweightStatus,
+  }
 
-    return res;
+  return res;
 }
